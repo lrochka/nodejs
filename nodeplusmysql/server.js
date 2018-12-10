6f4559d1b0b829be41bbdf8412b3a0b2a40c4eb4
lrochka <afanasievaiv@ya.ru>
2018-12-11 01:51:24 +0200

    Update server.js

--- nodeplusmysql/server.js
+++ nodeplusmysql/server.js
@@ -1,11 +1,6 @@
 const express = require('express')
 const app = express()
 
-app.get('/', function (req, res) {
-  alert("fucking message")
-  res.send('Hello World!')
-})
-
 app.listen(process.env.PORT, function () {
   console.log('Example app listening on port 3000!')
 })

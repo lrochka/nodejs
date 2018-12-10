91d68e173d86ccffdc03e714c2fe32e7a9bf72c7
lrochka <afanasievaiv@ya.ru>
2018-12-11 00:52:43 +0200

    Update db.js

--- nodeplusmysql/myapp/db.js
+++ nodeplusmysql/myapp/db.js
@@ -3,7 +3,6 @@ const mysql = require('mysql');
 //local mysql db connection
 const connection = mysql.createConnection({
     host     : 'sql12.freemysqlhosting.net',
-    port	: 3306,
     user     : 'sql12269468',
     password : 'wKgefULmLb',
     database : 'sql12269468'

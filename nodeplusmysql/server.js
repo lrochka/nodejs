75c36cef2755adf4f84fe77f3ea49c9142c8307d
lrochka <afanasievaiv@ya.ru>
2018-12-11 09:43:38 +0200

    Update server.js

--- nodeplusmysql/server.js
+++ nodeplusmysql/server.js
@@ -2,6 +2,17 @@ const express = require('express')
 const app = express()
 var mysql = require('mysql');
 
+var con = mysql.createConnection({
+  host: "zanner.org.ua",
+  user: "user",
+  password: "123456789"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+
 app.get('/', function(req, res) {
     
     res.sendfile('./index.html');

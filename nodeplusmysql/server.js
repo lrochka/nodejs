c5c03c40fc0d0d986829c4c585e66edc0292da95
lrochka <afanasievaiv@ya.ru>
2018-12-11 09:23:17 +0200

    Update server.js

--- nodeplusmysql/server.js
+++ nodeplusmysql/server.js
@@ -1,9 +1,8 @@
 const express = require('express')
 const app = express()
 
-app.get('/', function (req, res) {
-  
-  res.send("index.html")
+app.get('/', function(req, res) {
+    res.sendfile('./index.html');
 })
 
 app.listen(process.env.PORT, function () {

7c13a94113c25817ab1c6b5047b89b70ebe22ae2
lrochka <afanasievaiv@ya.ru>
2018-12-11 10:23:36 +0200

    Update server.js

--- nodeplusmysql/server.js
+++ nodeplusmysql/server.js
@@ -1,19 +1,50 @@
 const express = require('express')
 const app = express()
 var mysql = require('mysql');
-
-var con = mysql.createConnection({
+var pool = mysql.createPool({
   host: "zanner.org.ua",
   user: "user",
   password: "123456789"
 });
 
-app.use(
-  con.connect(function(err) {
+exports.connection = {
+    query: function () {
+        var queryArgs = Array.prototype.slice.call(arguments),
+            events = [],
+            eventNameIndex = {};
+
+        pool.getConnection(function (err, conn) {
+            if (err) {
+                if (eventNameIndex.error) {
+                    eventNameIndex.error();
+                }
+            }
+            if (conn) { 
+                var q = conn.query.apply(conn, queryArgs);
+                q.on('end', function () {
+                    conn.release();
+                });
+
+                events.forEach(function (args) {
+                    q.on.apply(q, args);
+                });
+            }
+        });
+
+        return {
+            on: function (eventName, callback) {
+                events.push(Array.prototype.slice.call(arguments));
+                eventNameIndex[eventName] = callback;
+                return this;
+            }
+        };
+    }
+};
+
+exports.connection.getConnection(function(err) {
   if (err) throw err;
   console.log("Connected!");
 });
-)
 
 app.get('/', function(req, res) {
     
